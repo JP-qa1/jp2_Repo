c80b2e07af84dce0a91aa9ebc40406127440574f
JP-qa1 <jsp_us@yahoo.com>
2023-03-29 22:49:59 -0400

    1st push into Jenkins

---
+++ JPMavenGitHubJenkinsTest2/src/test/java/JPMavenGitHubJenkinsTest2pkg/JPTest2.java
@@ -0,0 +1,14 @@
+package JPMavenGitHubJenkinsTest2pkg;
+
+import org.testng.annotations.Test;
+
+public class JPTest2
+
+{
+	@Test(groups= {"RegressionTest"})
+	public void method1()
+	{
+		System.out.println("Method2 is running");
+		System.out.println("-----");
+	}
+}
---
+++ JPMavenGitHubJenkinsTest2/src/test/java/JPMavenGitHubJenkinsTest2pkg/package-info.java
@@ -0,0 +1,1 @@
+package JPMavenGitHubJenkinsTest2pkg;
